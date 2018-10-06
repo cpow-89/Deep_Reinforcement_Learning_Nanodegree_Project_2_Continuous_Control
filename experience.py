77bc64a9ca09034900f942f9ef510469d411bf25
cpow-89 <christoph_powazny@gmx.de>
2018-10-06 23:25:33 +0200

    BigFix: to(self.seed) -> to(self.device) + set seed as optional argument # type of actions from long to float

--- experience.py
+++ experience.py
@@ -7,7 +7,7 @@ from collections import namedtuple, deque
 class ReplayBuffer:
     """Fixed-size experience buffer"""
 
-    def __init__(self, buffer_size, device, seed):
+    def __init__(self, buffer_size, device, seed=0):
         """Initialize a ReplayBuffer object."""
         self.memory = deque(maxlen=buffer_size)
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
@@ -23,11 +23,11 @@ class ReplayBuffer:
         """Randomly sample a batch of experiences from memory."""
         experiences = random.sample(self.memory, k=batch_size)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.seed)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(self.seed)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.seed)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.seed)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.seed)
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
 
         return states, actions, rewards, next_states, dones
 
