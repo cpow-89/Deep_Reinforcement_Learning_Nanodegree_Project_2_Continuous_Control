0c8d85b21efc84b4bef8fb4ecfd00019f5ce357d
cpow-89 <christoph_powazny@gmx.de>
2018-10-08 19:29:36 +0200

    - set up train and evaluation session

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 import json
-import session
-import torch
 from unityagents import UnityEnvironment
+
+import session
+from noise import OrnsteinUhlenbeckNoise
+from experience import ReplayBuffer
+from agent import DDPGAgent
+import utils
 
 
 def main():
@@ -10,12 +14,22 @@ def main():
         config = json.load(read_file)
 
     env = UnityEnvironment(file_name=os.path.join(*config["env_path"]))
-    device = torch.device(config["device"])
+
+    noise = OrnsteinUhlenbeckNoise(config["n_actions"], config["mu"], config["theta"], config["sigma"], config["seed"])
+    replay_buffer = ReplayBuffer(config["buffer_size"], config["device"], config["seed"])
+
+    agent = DDPGAgent(config, noise, replay_buffer)
 
     if config["run_training"]:
-        pass
+        session.train(agent, env, config)
+        checkpoint_dir = os.path.join(".", *config["checkpoint_dir"], config["env_name"])
+        utils.save_state_dict(os.path.join(checkpoint_dir, "actor"), agent.actor.state_dict())
+        utils.save_state_dict(os.path.join(checkpoint_dir, "critic"), agent.critic.state_dict())
     else:
-        session.test(None, env, 1)
+        checkpoint_dir = os.path.join(".", *config["checkpoint_dir"], config["env_name"])
+        agent.actor.load_state_dict(utils.load_latest_available_state_dict(os.path.join(checkpoint_dir, "actor", "*")))
+        agent.critic.load_state_dict(utils.load_latest_available_state_dict(os.path.join(checkpoint_dir, "critic", "*")))
+        session.evaluate(agent, env, num_test_runs=1)
 
     env.close()
 
--- session.py
+++ session.py
@@ -1,7 +1,42 @@
 import numpy as np
+import protocol
 
 
-def test(agent, env, num_test_runs=3):
+def _run_train_episode(agent, env, brain_name, train_log):
+    env_info = env.reset(train_mode=True)[brain_name]
+    state = env_info.vector_observations[0]
+    while True:
+        action = agent.act_noisy(state)
+        env_info = env.step(action)[brain_name]
+        next_state = env_info.vector_observations[0]  # get the next state
+        reward = env_info.rewards[0]  # get the reward
+        done = env_info.local_done[0]
+        agent.save_experiences(state, action, reward, next_state, done)
+        actor_loss, critic_loss = agent.learn()
+        state = next_state
+        train_log.step_update(reward, actor_loss, critic_loss)
+        if done:
+            break
+
+
+def train(agent, env, config):
+    # get the default brain
+    brain_name = env.brain_names[0]
+    train_log = protocol.Protocol(config)
+
+    for i_episode in range(1, config["max_num_episodes"] + 1):
+        _run_train_episode(agent, env, brain_name, train_log)
+        train_log.episode_update()
+        train_log.write_statistics()
+        print("\r", train_log, "\n", end="")
+
+        mean_reward = np.mean(np.mean(train_log.episode_rewards[-100:]))
+        if mean_reward >= config["average_score_for_solving"]:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100, mean_reward))
+            break
+
+
+def evaluate(agent, env, num_test_runs=3):
     brain_name = env.brain_names[0]
 
     for episode in range(num_test_runs):
@@ -9,9 +44,8 @@ def test(agent, env, num_test_runs=3):
         state = env_info.vector_observations[0]
         score = 0
         while True:
-            actions = np.random.randn(1, 4)  # select an action (for each agent)
-            actions = np.clip(actions, -1, 1)  # all actions between -1 and 1
-            env_info = env.step(actions)[brain_name]
+            action = agent.act(state)
+            env_info = env.step(action)[brain_name]
             next_state = env_info.vector_observations[0]
             reward = env_info.rewards[0]
             done = env_info.local_done[0]
@@ -20,4 +54,4 @@ def test(agent, env, num_test_runs=3):
             if done:
                 break
 
-    print("Score at Episode {}: {}".format(episode, score))
+        print("Score at Episode {}: {}".format(episode, score))
